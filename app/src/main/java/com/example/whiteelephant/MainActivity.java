74d34a17b97b1f4e0b04ed93a8a78b1765ad145d
Kolovratik <Kolovratik@HOME-PC>
2024-09-29 11:00:10 +0300

    Разработанная функция AlertDialog

--- app/src/main/java/com/example/whiteelephant/MainActivity.java
+++ app/src/main/java/com/example/whiteelephant/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.whiteelephant;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.os.Binder;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void AlertDialog(String title, String message){
+        AlertDialog.Builder Builder = new AlertDialog.Builder(MainActivity.this);
+        Builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
 
+        AlertDialog Alert = Builder.create();
+        Alert.show();
+    }
 }
